9615917d42c9eee490351e16369e15ad434c0e22
Sage911 <stefan.age@outlook.com>
2017-06-28 13:39:06 -0500

    Added enzyme, react-test-renderer to dependencies
    enzyme: airbnb’s package for testing component output.
    react-test-renderer: used for snapshot testing

---
+++ src/components/tests/Navbar.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Navbar from '../Navbar'
+
+
+test("Navbar should show 'FleetMED'", () => {
+
+  const navbar = shallow(
+    <Navbar />
+  )
+
+  expect(navbar.text()).toEqual('FleetMED')
+
+})
