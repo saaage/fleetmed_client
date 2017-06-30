513dba0bbaaec2cc99d287e37daeaee9af9394d4
Sage911 <stefan.age@outlook.com>
2017-06-29 19:32:36 -0500

    Explaining @connect

--- src/components/containers/Reviews.js
+++ src/components/containers/Reviews.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { fetchReviews } from '../../actions/reviewActions'
 
-// @connect connects a React component to the Redux store
-// connect returns a new connected component class for us to use with our Redux store
+// @connect connects a React component to the Redux store allowing us to map state to props
+// the module export will include the connected component
 @connect((store) => {
   return {
     reviews: store.reviews.allReviews
@@ -20,7 +20,6 @@ class Reviews extends React.Component {
   }
 
   componentDidMount() {
-    console.log(this.props);
   }
 
   render() {
